6f3dc2b3cb0602a19a2a54480268d34ff107b165
zain975 <zain97531@gmail.com>
2026-01-06 13:10:23 +0500

    update-database-schema

--- prisma/migrations/20251118091602_add_account_privacy_and_social_links/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateEnum
-CREATE TYPE "AccountPrivacy" AS ENUM ('PUBLIC', 'PRIVATE');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "accountPrivacy" "AccountPrivacy" NOT NULL DEFAULT 'PUBLIC',
-ADD COLUMN     "instagramUrl" TEXT,
-ADD COLUMN     "tiktokUrl" TEXT,
-ADD COLUMN     "xUrl" TEXT;
